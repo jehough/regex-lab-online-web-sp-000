0b1d874eafdf02c34d76e43623eb7b36a4a185ad
Jim Hough <teachlife@outlook.com>
2019-04-18 03:11:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  (text.match /[\AA-Z]/) && (text.match /\W\z/)
+  (!!text.match /[\AA-Z]/) && (!!text.match /\W\z/)
 end
 
 def valid_phone_number?(phone)
