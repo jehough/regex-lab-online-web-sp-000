225e9c28e90614c512c733642462b3286ce4c658
Jim Hough <teachlife@outlook.com>
2019-04-18 03:26:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text == /[\AA-Z]/
-    text == /\W\b/
+  /[\AA-Z]/.match?(text) 
   end
 end
 
